ef3ca8de2f23d867aed47df7321909258fbd8cd0
liujiandong <liujiandong@iprexmed.com>
2017-11-13 15:50:53 +0800

    finish 0.1.0

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
+	"sync"
 )
 
 var xList = map[string]string{
@@ -31,16 +35,102 @@ func main() {
 	// 获取 gopath
 	var gopathEnv string = os.Getenv("GOPATH")
 	gopaths := strings.Split(gopathEnv, ":")
+	if len(gopaths) == 0 {
+		panic("please set your gopath! like: export GOPATH=/your/gopath")
+	}
+	gopath := gopaths[0]
+	fmt.Println(gopath)
 
-	// 检查 gopath 是否存在相关库
-	var exist bool
-	for _, gopath := range gopaths {
-		fmt.Println(gopath)
+	// 创建目录$GOPATH/src/golang.org/x/
+	fileDir := filepath.Join(gopath, "src", "golang.org", "x")
+	_, err := os.Stat(fileDir)
+	if err != nil && os.IsNotExist(err) {
+		err = os.MkdirAll(fileDir, 0755)
+		if err != nil {
+			panic(fmt.Sprintf("创建目录失败: %s, err: %v\n", fileDir, err))
+		}
 	}
 
-	if !exist {
-		// 获取
-	} else {
-		// 更新
+	wg := new(sync.WaitGroup)
+	for key, link := range xList {
+		file := filepath.Join(fileDir, key)
+
+		// 是否存在相关库目录
+		_, err = os.Stat(file)
+		if err != nil {
+			if os.IsNotExist(err) {
+				// 目录不存在，添加
+				wg.Add(1)
+				go func(fileDir, link string) {
+					defer wg.Done()
+					err = cloneGox(fileDir, link)
+					if err != nil {
+						fmt.Printf("clone gox failed, link: %s, err: %v\n", link, err)
+					}
+				}(fileDir, link)
+			}
+		} else if fis, err := ioutil.ReadDir(file); err == nil && len(fis) == 0 {
+			// 空目录，添加
+			wg.Add(1)
+			go func(fileDir, link string) {
+				defer wg.Done()
+				err = cloneGox(fileDir, link)
+				if err != nil {
+					fmt.Printf("clone gox failed, link: %s, err: %v\n", link, err)
+				}
+			}(fileDir, link)
+		} else {
+			// 目录不为空，更新
+			wg.Add(1)
+			go func(file, link string) {
+				defer wg.Done()
+				err = pullGox(file, link)
+				if err != nil {
+					fmt.Printf("pull gox failed, link: %s, err: %v\n", link, err)
+				}
+			}(file, link)
+		}
 	}
+
+	wg.Wait()
+}
+
+func cloneGox(file, link string) error {
+	// cd [file]
+	err := os.Chdir(file)
+	if err != nil {
+		return err
+	}
+
+	// git clone [link]
+	fmt.Printf("begin clone '%s'\n", link)
+	cmd := exec.Command("git", "clone", link)
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("%s\n", output)
+		return err
+	}
+	fmt.Printf("finish clone '%s'\n", link)
+
+	return nil
+}
+
+func pullGox(file, link string) error {
+	// cd [file]
+	err := os.Chdir(file)
+	if err != nil {
+		return err
+	}
+
+	// git pull
+	fmt.Printf("begin pull '%s'\n", link)
+	cmd := exec.Command("git", "pull")
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("%s\n", output)
+		return err
+	}
+	fmt.Printf("finish pull '%s'\n", link)
+
+	return nil
 }
